17c7325fad1438ed1839004670a3c6ad87c6034f
Albuquerquess <albuquerque.develop@gmail.com>
2021-06-22 16:07:05 -0300

    crete  tmp payment info migration file

---
+++ src/Database/migrations/payment/20210622190454_a.ts
@@ -0,0 +1,18 @@
+import {Knex} from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('TmpPaymentInfos', (table: Knex.TableBuilder) => {
+        table.increments('id').primary()
+        table.string('name').notNullable()
+        table.string('email').notNullable()
+        table.string('phone').notNullable()
+        table.timestamp('registered_at').defaultTo(knex.fn.now())
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema
+    .dropTable('TmpPaymentInfos')
+}
+
