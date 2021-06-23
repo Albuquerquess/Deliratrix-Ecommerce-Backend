b846331f7be9f4d63a945a35581b8003921641af
Albuquerquess <albuquerque.develop@gmail.com>
2021-06-23 13:04:32 -0300

    FIX - database file path

--- src/Database/connections/content/Products/productConnections.ts
+++ src/Database/connections/content/Products/productConnections.ts
@@ -6,7 +6,7 @@ import { PRODUCSTDATABASENAME } from '../../../../Consts/DBConsts'
 const productsConnection = knex({
   client: 'sqlite3',
   connection: {
-    filename: path.resolve(__dirname, '..', '..', '..', 'DB', 'Content', PRODUCSTDATABASENAME)
+    filename: path.resolve(__dirname, '..', '..', '..', 'DB', 'Payment', PRODUCSTDATABASENAME)
   },
   useNullAsDefault: true,
 });
