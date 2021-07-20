b37862b7788f19bfeae24654bbc77c0d3ea05f62
Albuquerquess <albuquerque.develop@gmail.com>
2021-07-20 10:10:25 -0300

    Update tmp database

--- src/Database/migrations/tmp/20210628171418_cart.ts
+++ src/Database/migrations/tmp/20210628171418_cart.ts
@@ -1,16 +1,28 @@
 import {Knex} from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.createTable('cart', (table: Knex.TableBuilder) => {
-        table.increments('id').primary()
-        table.string('id_content').notNullable()
-        table.string('txid').notNullable()
+    return knex.schema.createTable('tmp_debtor', (table: Knex.TableBuilder) => {
+        table.increments('debtor_id').primary()
         table.string('name').notNullable()
-        table.integer('value').notNullable()
-        table.timestamp('registered_at').defaultTo(knex.fn.now())
+        table.string('phone').notNullable()
+        table.string('email').notNullable()
+        table.string('txid').notNullable()
     })
-}
 
+    .createTable('tmp_cart', (table: Knex.TableBuilder) => {
+        table.increments('id').primary()
+        table.integer('price').notNullable()
+        table.string('content_id').notNullable()
+        table.string('debtor_id').notNullable()
+        table.string('txid').notNullable()
+        
+        table.timestamp('registered_at').defaultTo(knex.fn.now())
+        table.foreign('debtor_id').references('id').inTable('debtor')
+    })
+    
+
+    
+}
 
 export async function down(knex: Knex): Promise<void> {
     return knex.schema
