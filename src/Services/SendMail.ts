b2415d81282b446c87f6eb221184795ab922fa8f
app-code-commit-at-882761754857 <albuquerque.develop@gmail.com>
2021-08-10 15:12:56 -0300

    change contents links index

--- src/Services/SendMail.ts
+++ src/Services/SendMail.ts
@@ -38,7 +38,7 @@ async function sendMail(debtor: debtorProps, finalContentsUrl: finalContentsUrlP
          </ul>
          <span><strong>Link com os conteúdos</strong></span>
          <ul>
-           ${finalContentsUrl.map(finalContentUrl => `<li>Titulo 03: <a href="${finalContentUrl.url}">Acessar conteúdo</a> ID: ${finalContentUrl.create_id}</li>`)}
+           ${finalContentsUrl.map((finalContentUrl, index) => `<li>${index}: <a href="${finalContentUrl.url}">Acessar conteúdo</a> ID: ${finalContentUrl.create_id}</li>`)}
          </ul>
        </main>
       </div>`, // html body
